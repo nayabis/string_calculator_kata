5b51a2df5facb8c28836cbb14883026cef613efe
BISWAJIT NAYAK <bnayak.nayak7@gmail.com>
2024-12-28 00:28:20 +0530

    Support custom delimiters | Refactored the code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,29 @@
 class StringCalculator
-  def self.add(numbers)
-    return 0 if numbers.empty?
-    return numbers.to_i if numbers.to_i.to_s == numbers
-    numbers.split(/,|\n/).map(&:to_i).sum
+  def self.add(input)
+    return 0 if input.empty?
+  
+    numbers = parse_input(input)
+    numbers.sum
   end
+  
+  private
+
+  def self.parse_input(input)
+    if input.start_with?("//")
+      delimiter, numbers_part = extract_custom_delimiter(input)
+      split_numbers(numbers_part, delimiter)
+    else
+      split_numbers(input, /,|\n/)
+    end
+  end
+  
+  def self.extract_custom_delimiter(input)
+    delimiter = input[2]
+    numbers_part = input[4..]
+    [delimiter, numbers_part]
+  end
+  
+  def self.split_numbers(input, delimiter)
+    input.split(delimiter).map(&:to_i)
+  end  
 end
