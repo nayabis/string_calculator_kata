6b78c087935a6ffa58ce1745973bb440c0af2c3d
BISWAJIT NAYAK <bnayak.nayak7@gmail.com>
2024-12-28 00:04:35 +0530

    converted add method to a class method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,5 @@
 class StringCalculator
-  def add(numbers)
+  def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.to_i.to_s == numbers
     numbers.split(",").map(&:to_i).sum
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,23 +3,19 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
   describe '#add' do
     it 'returns 0 for an empty string' do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq(0)
+      expect(described_class.add("")).to eq(0)
     end
 
     it 'returns the number itself for a single number' do
-      calculator = StringCalculator.new
-      expect(calculator.add("5")).to eq(5)
+      expect(described_class.add("5")).to eq(5)
     end
 
     it 'returns the sum of two numbers separated by a comma' do
-      calculator = StringCalculator.new
-      expect(calculator.add("1,2")).to eq(3)
+      expect(described_class.add("1,2")).to eq(3)
     end
 
     it "returns the sum of multiple numbers" do
-      calculator = StringCalculator.new
-      expect(calculator.add("1,2, 3, 6, 8")).to eq(20)
+      expect(described_class.add("1,2, 3, 6, 8")).to eq(20)
     end
   end
 end
